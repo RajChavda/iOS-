78e3cfcdc75f7f6d5c1428d66d671000f8d83373
RajChavda <rajchavda2511@gmail.com>
2016-07-15 10:38:35 +0530

    First
    Hello This is the first commit for testing

--- Pinky/AppDelegate.m
+++ Pinky/AppDelegate.m
@@ -7,15 +7,43 @@
 //
 
 #import "AppDelegate.h"
-
+#import "AFNetworking.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [self loadJson];
     return YES;
 }
-							
+
+
+-(void)loadJson
+{
+    {
+        NSMutableDictionary *dict =[[NSMutableDictionary alloc]init];
+        
+        [dict setValue:@"555" forKey:@"plate_name"];
+        [dict setValue:@"parkmeter" forKey:@"option"];
+        [dict setValue:@"6e203720924a0a59806356bc0e3a46406d3ca09ec202185d9c4f288a72e01de1" forKey:@"device_id"];
+        [dict setValue:@"9999999999" forKey:@"phone_no"];
+        [dict setValue:@"G" forKey:@"symbol"];
+        
+        NSString *base_url =[NSString stringWithFormat:@"http://ilebanon.me/searchapp/updated_code-1.3/parkEn.php?"];
+        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+        //  NSDictionary *parameters = @{@"foo": @"bar"};
+        [manager POST:base_url parameters:dict success:^(AFHTTPRequestOperation *operation, id responseObject)
+         {
+             NSLog(@"responseObject in view did load==%@",responseObject);
+             NSDictionary *dictResponse = responseObject;
+
+         }
+              failure:^(AFHTTPRequestOperation *operation, NSError *error)
+         {
+             NSLog(@"Error: %@", error);
+         }];
+    }
+}
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '7.0'
+
+target 'Pinky' do
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '7.0'
+pod 'AFNetworking'
+
+end
+
+target 'PinkyTests' do
+
+end
+
